813b44991dee960fa6c2b93746b0c57ced35ca8f
supriyo18 <mukherjee.supriyo18@gmail.com>
2024-12-17 19:10:24 +0530

    userModels & videoModels with access and JWT token

---
+++ src/models/youtube.models.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import { type } from "os";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String,
+            required: [true, 'Video file is required']
+        },
+        thumbnail: {
+            type: String,
+            required: [true, 'Thumbnail is required']
+        },
+        title: {
+            type: String,
+            required: [true, 'Title is required']
+        },
+        description: {
+            type: String,
+            required: [true, 'Description is required']
+        },
+        duration: {
+            type: Number,
+            required: [true, 'Duration is required']
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }, {
+    timestamps: true
+}
+)
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video", videoSchema)
